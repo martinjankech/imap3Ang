7d23e9a297b0eafadfa3d998a0e726dd963f1391
Martin Jankech <jankech3@gmail.com>
2021-02-19 10:38:45 +0100

    counter

---
+++ src/app/calc/calc.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calc',
+  templateUrl: './calc.component.html',
+  styles: [
+  ]
+})
+export class CalcComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
